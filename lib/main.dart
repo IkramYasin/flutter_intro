6345ff86ea939e48242412cc1691c3516f40f38b
Ikram Yasin <1110899@stud.uot.edu.pk>
2026-09-04 15:19:31 +0000

    message

--- lib/main.dart
+++ lib/main.dart
@@ -206,7 +206,13 @@ class _InitialScreenState extends State<initial_screen> {
                   children: [
                     Text("Don't have an account?", style: const TextStyle(color: Colors.white, fontSize: 16)),
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        // Navigate to the register screen
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const register_screen(title: 'Register Page')),
+                        );
+                      },
                       child: const Text('Register', style: TextStyle(color: Colors.white, fontSize: 16)),
                     ),
                   ],
@@ -232,125 +238,151 @@ class register_screen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<register_screen> {
+  bool isChecked = true; // Variable to track checkbox state
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text(widget.title, style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold))),
         backgroundColor: const Color.fromARGB(255, 0, 73, 157),
       ),
+      backgroundColor: const Color.fromARGB(255, 0, 57, 118),
       body: Center(
-        child: Card(
-            color: const Color.fromARGB(255, 0, 174, 255),
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                // Text('Welcome to the Initial Screen!', style: const TextStyle(color: Colors.white)),
-               SizedBox(height: 25),
-               
-                const CircleAvatar(
-                  radius: 70,
-                  backgroundImage: NetworkImage('https://cdn-icons-png.flaticon.com/512/3135/3135715.png'),
-                ),
-                const SizedBox(height: 15),
-            
-                Text('Welcome! Please Register', style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 15),
-                
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Name",
-                    hintText: 'Enter your name',
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 255, 255, 255),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color.fromARGB(255, 8, 8, 8)),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
+        child: Container(
+          height: 1000,
+          width: 700,
+          child: Card(
+              color: const Color.fromARGB(255, 0, 174, 255),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Text('Welcome to the Initial Screen!', style: const TextStyle(color: Colors.white)),
+                 SizedBox(height: 25),
+                 
+                  const CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage('https://cdn-icons-png.flaticon.com/512/3135/3135715.png'),
+                  ),
+                  const SizedBox(height: 15),
+              
+                  Text('Welcome! Please Register', style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 15),
+                  
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Name",
+                      hintText: 'Enter your name',
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 255, 255, 255),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color.fromARGB(255, 8, 8, 8)),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
                     ),
                   ),
-                ),
-            
-                const SizedBox(height: 15),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Email",
-                    hintText: 'Enter your email',
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 255, 255, 255),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color.fromARGB(255, 8, 8, 8)),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
+              
+                  const SizedBox(height: 15),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Email",
+                      hintText: 'Enter your email',
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 255, 255, 255),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color.fromARGB(255, 8, 8, 8)),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
                     ),
                   ),
-                ),
-            
-                const SizedBox(height: 15),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Password",
-                    hintText: 'Enter your password',
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 255, 255, 255),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color.fromARGB(255, 8, 8, 8)),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
+              
+                  const SizedBox(height: 15),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Password",
+                      hintText: 'Enter your password',
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 255, 255, 255),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color.fromARGB(255, 8, 8, 8)),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
                     ),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-
-                const SizedBox(height: 15),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Confirm Password",
-                    hintText: 'Enter your password again',
-                    filled: true,
-                    fillColor: Color.fromARGB(255, 255, 255, 255),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Color.fromARGB(255, 8, 8, 8)),
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
+          
+                  const SizedBox(height: 15),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Confirm Password",
+                      hintText: 'Enter your password again',
+                      filled: true,
+                      fillColor: Color.fromARGB(255, 255, 255, 255),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color.fromARGB(255, 8, 8, 8)),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
                     ),
+                    obscureText: true,
                   ),
-                  obscureText: true,
-                ),
-
-                CheckboxListTile(
-                  title: const Text("I agree to the Terms and Conditions", style: TextStyle(color: Colors.white, fontSize: 16)),
-                  value: false, // Replace with your actual checkbox state
-                  onChanged: (bool? value) {
-                    // Handle checkbox state change
-                  },
-                  controlAffinity: ListTileControlAffinity.leading,
-                ),          
-                const SizedBox(height: 15),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 0, 73, 157),
-                    padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+          
+                  CheckboxListTile(
+                    title: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text("I agree to the ", style: TextStyle(color: Colors.white, fontSize: 16)),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text("Terms and Conditions", style: TextStyle(color: Colors.white, fontSize: 16, decoration: TextDecoration.underline)),
+                        ),
+                      ],
                     ),
-                    minimumSize: const Size(double.infinity, 50), // Make the button full width
+                    value: isChecked, 
+                    onChanged: (bool? value) {
+                      setState(() {
+                        // Handle checkbox state change
+                        isChecked = value ?? false;
+                      });
+                    },
+                    controlAffinity: ListTileControlAffinity.leading,
+                  ),          
+                  const SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle register button press
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 0, 73, 157),
+                      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      minimumSize: const Size(double.infinity, 50), // Make the button full width
+                    ),
+                    child: const Text('Register', style: TextStyle(color: Colors.white, fontSize: 16)),
                   ),
-                  child: const Text('Register', style: TextStyle(color: Colors.white, fontSize: 16)),
-                ),
-
-                
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Already have an account?", style: const TextStyle(color: Colors.white, fontSize: 16)),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text('Login', style: TextStyle(color: Colors.white, fontSize: 16)),
-                    ),
-                  ],
-                ),
-            
-              ],
+          
+                  
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Already have an account?", style: const TextStyle(color: Colors.white, fontSize: 16)),
+                      TextButton(
+                        onPressed: () {
+                          // Navigate to the login screen
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const initial_screen(title: 'Login Page')),
+                          );
+                        },
+                        child: const Text('Login', style: TextStyle(color: Colors.white, fontSize: 16)),
+                      ),
+                    ],
+                  ),
+              
+                ],
+              ),
             ),
-          ),
+        ),
       )
     );
   }
